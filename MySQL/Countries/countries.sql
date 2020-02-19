bdba8cd4bd8c60c70d505ae0d3cf19ebca893bf7
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-02-19 09:01:43 -0300

    countries SQL

--- MySQL/Countries/countries.sql
+++ MySQL/Countries/countries.sql
@@ -16,7 +16,7 @@ ORDER BY l.percentage DESC;
 -- Consulta 2
 
 SELECT 
-    c.name AS Pais, COUNT(ci.id)
+    c.name AS Pais, COUNT(ci.id) as Poblacion
 FROM
     countries AS c
         INNER JOIN
@@ -28,7 +28,7 @@ ORDER BY COUNT(ci.id) DESC;
 -- Consulta 3
 
 SELECT 
-    ci.name, ci.population
+    ci.name as Ciudad, ci.population as Poblacion
 FROM
     cities AS ci
         INNER JOIN
@@ -39,3 +39,67 @@ WHERE
 ORDER BY ci.population DESC;
 
 -- Consulta 4
+
+SELECT 
+    c.name AS Pais,
+    l.language AS Idioma,
+    l.percentage AS Porcentaje
+FROM
+    languages AS l
+        INNER JOIN
+    countries AS c ON l.country_id = c.id
+ WHERE
+    l.percentage > 89
+GROUP BY l.language
+ORDER BY l.percentage DESC;
+
+-- Consulta 5
+
+SELECT 
+    c.name AS Pais,
+    c.population AS Poblacion,
+    c.surface_area AS Superficie
+FROM
+    countries AS c
+WHERE
+    surface_area < 501
+        AND population > 100000;
+        
+-- Consulta 6
+
+SELECT 
+    name AS Pais,
+    government_form AS 'Forma de gobierno',
+    capital,
+    life_expectancy AS 'Esperanza de vida'
+FROM
+    countries
+WHERE
+    government_form LIKE 'Constitutional Monarchy%'
+        AND capital > 200
+        AND life_expectancy > 75
+ORDER BY name;
+
+-- Consulta 7
+
+SELECT 
+    c.name AS Pais,
+    ci.name AS Ciudad,
+    ci.district AS Distrito,
+    ci.population AS Poblacion
+FROM
+    cities AS ci
+        INNER JOIN
+    countries AS c ON ci.country_id = c.id
+WHERE
+    ci.district = 'Buenos Aires'
+        AND ci.population > 500000;
+    
+-- Consulta 8
+
+SELECT 
+    region AS Región, COUNT(id) AS 'Número de paises'
+FROM
+    countries
+GROUP BY region
+ORDER BY COUNT(id);
