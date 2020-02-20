da627ccd4dff6a1ad5f7f3bcd03dd048794c3a10
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-02-20 12:24:26 -0300

    Pitagoras

---
+++ Java/Pitágoras/src/Pitagoras.java
@@ -0,0 +1,6 @@
+
+public class Pitagoras {
+	public double calculateHypotenuse(int legA, int legB) {
+		return Math.sqrt(Math.pow(legA, 2)+Math.pow(legB, 2));
+	}
+}
---
+++ Java/Pitágoras/src/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Pitagoras p = new Pitagoras();
+		System.out.println("La hipotenusa de un triángulo con catetos 3 y 4 es: "+p.calculateHypotenuse(3, 4));
+	}
+}
