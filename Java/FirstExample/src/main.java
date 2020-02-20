90a2455d6f4e26882d95dca218b70a7ced0adae5
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-02-20 12:15:31 -0300

    first program java

---
+++ Java/FirstExample/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Mi nombre es: Gustavo\n"
+				+ "Tengo 25 años de edad\n"
+				+ "Mi ciudad es Temuco, Chile.");
+	}
+
+}
