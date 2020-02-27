41601aa37ac325da683fe7b191b95c8419e27795
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-02-27 13:30:46 -0300

    portafolio

---
+++ Java/Proyecto class/src/Portfolio.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Portfolio {
+
+	//Atributo
+	private ArrayList<Project> projects;
+
+	//Getters y Setters
+	public ArrayList<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(ArrayList<Project> projects) {
+		this.projects = projects;
+	}
+
+	//Constructor
+	public Portfolio(ArrayList<?> projects) {
+		for (Object object : projects) {
+			if(object instanceof Project)
+				this.projects.add((Project)object);
+		}
+	}
+
+	//Métodos	
+	public double getPortfolioCost(){
+		double total=0;
+		for (Project project : projects) {
+			total+=project.getInitialCost();
+		}
+		return total;
+	}
+
+	public void showPortfolio() {
+		for (Project project : this.projects) {
+			System.out.println(project.elevatorPitch());
+		}
+		System.out.println("El costo total es: " + this.getPortfolioCost());
+	}
+}
---
+++ Java/Proyecto class/src/Project.java
@@ -0,0 +1,51 @@
+
+public class Project {
+
+	// Atributos
+	private String name;
+	private String description;
+	private double initialCost;
+
+	public double getInitialCost() {
+		return initialCost;
+	}
+
+	public void setInitialCost(double initialCost) {
+		this.initialCost = initialCost;
+	}
+
+	//Getters y Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// Constructores
+	public Project() { }
+	
+	public Project(String name) { 
+		this.name = name;
+	}
+	
+	public Project(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	//Métodos
+	public String elevatorPitch() {
+		return (name + " ("+ initialCost +"): " + description);
+	}
+
+}
