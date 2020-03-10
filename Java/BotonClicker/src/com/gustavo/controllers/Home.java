20286572434738a07356bceec800eb5d8dd7350d
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-03-10 12:06:51 -0300

    BotonClicker

---
+++ Java/BotonClicker/src/com/gustavo/controllers/Home.java
@@ -0,0 +1,46 @@
+package com.gustavo.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.gustavo.models.*;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		HttpSession session = request.getSession();
+		User user;
+		if(session.getAttribute("clicks") != null)
+			user = new User((Integer)session.getAttribute("clicks"));
+		else
+			user = new User(0);
+		session.setAttribute("clicks", user.getClicks());	
+		request.setAttribute("user", user);
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
