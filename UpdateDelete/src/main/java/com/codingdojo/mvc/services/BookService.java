eaa0dad9d16c3019512d11ee8f8a3e872b60ba5b
Gustavo Vigueras <g.vigueras01@ufromail.cl>
2020-07-08 12:25:27 -0400

    updateDelete full

---
+++ UpdateDelete/src/main/java/com/codingdojo/mvc/services/BookService.java
@@ -0,0 +1,43 @@
+package com.codingdojo.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	//Agregando el book al repositorio como una dependencia
+	private final BookRepository bookRepository;
+
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	//Devolviendo todos los libros.
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+	//Creando un libro.
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+	//Obteniendo la información de un libro
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if(optionalBook.isPresent()) {
+			return optionalBook.get();
+		} else {
+			return null;
+		}
+	}
+	//Actualiza el libro dado
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		return bookRepository.save(new Book(title, desc, lang, numOfPages));
+	}
+	//Eliminando un libro
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+}
